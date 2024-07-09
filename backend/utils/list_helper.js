6cc7553432ae3b05b0b89c4b9da7657f4b8d8561
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-07-09 22:29:08 +0200

    4.5

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -5,7 +5,23 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
+
+const favoriteBlog = (blogs) => {
+  let highestLikes = 0;
+  let favorite = null;
+
+  for (const blog of blogs) {
+    if (blog.likes > highestLikes) {
+      highestLikes = blog.likes;
+      favorite = blog;
+    }
+  }
+
+  return favorite;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
